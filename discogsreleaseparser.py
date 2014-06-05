801e1aa0db8d2c6d385e1444d336ff455ec3bf70
ijabz <ijabz@fastmail.fm>
2014-06-05 22:36:32 +0100

    Revert companies change, fails

--- discogsreleaseparser.py
+++ discogsreleaseparser.py
@@ -31,6 +31,8 @@ class ReleaseHandler(xml.sax.handler.ContentHandler):
 							'identifier',
 							'videos',
 							'video',
+						        'companies',
+							'company',		
 							'catno',
 							'entity_type',
 							'entity_type_name',

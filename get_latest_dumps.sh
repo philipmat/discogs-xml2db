c7a3f41d44fe90487b04ad79f28d0bb7e0f4a3eb
Ubuntu <ubuntu@ip-172-31-47-111.eu-west-1.compute.internal>
2014-07-01 16:14:29 +0000

    fix typo with label

--- get_latest_dumps.sh
+++ get_latest_dumps.sh
@@ -1,4 +1,5 @@
 #/bin/bash
+#set -xv
 USER_AGENT="Mozilla/5.0 (Macintosh; Intel Mac OS X 10_7_2) AppleWebKit/534.51.22 (KHTML, like Gecko) Version/5.1.1 Safari/534.51.22" 
 ACCEPT="Accept-Encoding: gzip, deflate" 
 D_URL="http://www.discogs.com/data/"

89a5ab435b08870a91bdb127e65c8a68703fc8f1
Xavier Godart <xavier@blitzr.com>
2014-08-19 16:34:36 +0200

    Add path arguments to fix-xml script

--- get_latest_dumps.sh
+++ get_latest_dumps.sh
@@ -1,13 +1,13 @@
 #/bin/bash
 #set -xv
-USER_AGENT="Mozilla/5.0 (Macintosh; Intel Mac OS X 10_7_2) AppleWebKit/534.51.22 (KHTML, like Gecko) Version/5.1.1 Safari/534.51.22" 
-ACCEPT="Accept-Encoding: gzip, deflate" 
+USER_AGENT="Mozilla/5.0 (Macintosh; Intel Mac OS X 10_7_2) AppleWebKit/534.51.22 (KHTML, like Gecko) Version/5.1.1 Safari/534.51.22"
+ACCEPT="Accept-Encoding: gzip, deflate"
 D_URL="http://www.discogs.com/data/"
 D_TMP=/tmp/discogs.urls
 D_PATTERN="discogs_\d+_(artists|labels|masters|releases).xml.gz"
 
 TEST=""
-[[ "$1" == '--test' ]] && TEST='--spider -S' 
+[[ "$1" == '--test' ]] && TEST='--spider -S'
 
 echo "" > $D_TMP
 
@@ -15,6 +15,4 @@ for f in `wget -c --user-agent="$USER_AGENT" --header="$ACCEPT" -qO- $D_URL | ac
 	echo $D_URL$f >> $D_TMP
 done
 
-wget -c --user-agent="$USER_AGENT" --header="$ACCEPT" --no-clobber --input-file=$D_TMP $TEST --append-output=$D_TMP.log 
-
-
+wget -c --user-agent="$USER_AGENT" --header="$ACCEPT" --no-clobber --input-file=$D_TMP $TEST --append-output=$D_TMP.log

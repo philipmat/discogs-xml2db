e2b60dab90f00d25ef2a4d4d24d816031c46c209
paul <ijabz@fastmail.fm>
2014-08-22 17:42:01 +0100

    put disocgs table in disocgs schema

--- create_tables.sql
+++ create_tables.sql
@@ -4,14 +4,14 @@ SET check_function_bodies = false;
 SET client_min_messages = warning;
 SET escape_string_warning = off;
 
-SET search_path = public, pg_catalog;
+SET search_path = discogs;
 
 SET default_tablespace = '';
 
 SET default_with_oids = false;
 
 SET synchronous_commit=off;
-
+ 
 CREATE UNLOGGED TABLE artist (
     id 				integer NOT NULL,
     name 			text NOT NULL,

c1ff689b97885d4297eb79b344fee6c68a63f0bf
Paul Tremberth <paul.tremberth@gmail.com>
2017-04-25 11:33:07 +0200

    PEP8-ify

--- postgresexporter.py
+++ postgresexporter.py
@@ -21,6 +21,7 @@ import sys
 
 from psycopg2.extensions import adapt, register_adapter
 
+
 def untuple(what):
 	if type(what) is tuple:
 		return list(what)
@@ -74,6 +75,7 @@ class PostgresExporter(object):
 			self.cur.close()
 
 	_query_cache = {}
+
 	def buildEscapedQuery(self, table, columns):
 		qkey = (table, tuple(columns))
 		q = self._query_cache.get(qkey)
@@ -119,8 +121,10 @@ class PostgresExporter(object):
 
 		for img in label.images:
 			values = (img.imageType, img.height, img.width, label.id)
-			self.runQuery('labels_images',
-				['type', 'height', 'width', 'label_id'], values)
+			self.runQuery(
+				'labels_images',
+				['type', 'height', 'width', 'label_id'],
+				values)
 
 	def storeArtist(self, artist):
 		if not self.good_quality(artist):
@@ -154,8 +158,10 @@ class PostgresExporter(object):
 
 		for img in artist.images:
 			values = (img.imageType, img.height, img.width, artist.id)
-			self.runQuery('artists_images',
-				['type', 'height', 'width', 'artist_id'], values)
+			self.runQuery(
+				'artists_images',
+				['type', 'height', 'width', 'artist_id'],
+				values)
 
 	def storeRelease(self, release):
 		if not self.good_quality(release):
@@ -166,7 +172,7 @@ class PostgresExporter(object):
 				('id', 'id'),
 				('title', 'title'),
 				('status', 'status'),
-				('barcode', 'barcode'),]:
+				('barcode', 'barcode')]:
 			values.append(getattr(release, attr))
 			columns.append(column)
 
@@ -193,8 +199,10 @@ class PostgresExporter(object):
 
 		for img in release.images:
 			values = (img.imageType, img.height, img.width, release.id)
-			self.runQuery('releases_images',
-				['type', 'height', 'width', 'release_id'], values)
+			self.runQuery(
+				'releases_images',
+				['type', 'height', 'width', 'release_id'],
+				values)
 
 		for fmt_order, fmt in enumerate(release.formats, start=1):
 			if len(release.formats) != 0:
@@ -205,39 +213,47 @@ class PostgresExporter(object):
 					except PostgresExporter.ExecuteError as e:
 						print("%s" % (e.args))
 				values = (release.id, fmt_order, fmt.name, fmt.qty, fmt.descriptions)
-				self.runQuery('releases_formats',
-					['release_id', 'position', 'format_name', 'qty', 'descriptions'], values)
+				self.runQuery(
+					'releases_formats',
+					['release_id', 'position', 'format_name', 'qty', 'descriptions'],
+					values)
 
 		for lbl in release.labels:
-			self.runQuery('releases_labels',
+			self.runQuery(
+				'releases_labels',
 				['release_id', 'label', 'catno'],
 				(release.id, lbl.name, lbl.catno))
 
 		for aj_pos, aj in enumerate(release.artistJoins, start=1):
-			self.runQuery('releases_artists',
+			self.runQuery(
+				'releases_artists',
 				['release_id', 'position', 'artist_id', 'artist_name', 'join_relation', 'anv'],
 				(release.id, aj_pos, aj.artist_id, aj.artist_name, aj.join_relation, aj.anv))
 
 		for extr in release.extraartists:
 			for role in extr.roles:
-				self.runQuery('releases_extraartists',
-					['release_id', 'artist_id',     'artist_name',    'role', 'anv'],
+				self.runQuery(
+					'releases_extraartists',
+					['release_id', 'artist_id', 'artist_name', 'role', 'anv'],
 					(release.id, extr.artist_id, extr.artist_name, role, extr.anv))
 
 		for trackno, trk in enumerate(release.tracklist, start=1):
 			trackid = str(uuid.uuid4())
-			self.runQuery('track',
+			self.runQuery(
+				'track',
 				['release_id', 'title', 'duration', 'position', 'track_id', 'trackno'],
 				(release.id, trk.title, trk.duration, trk.position, trackid, trackno))
 
 			for track_artist_order, aj in enumerate(trk.artistJoins, start=1):
-				self.runQuery('tracks_artists',
+				self.runQuery(
+					'tracks_artists',
 					['track_id', 'position', 'artist_name', 'artist_id', 'join_relation', 'anv'],
 					(trackid, track_artist_order, aj.artist_name, aj.artist_id, aj.join_relation, aj.anv))
 
 			for extr in trk.extraartists:
 				for role in extr.roles:
-					self.runQuery('tracks_extraartists',
+					self.runQuery(
+						'tracks_extraartists',
 						['track_id', 'artist_id', 'artist_name', 'role', 'anv'],
 						(trackid, extr.artist_id, extr.artist_name, role, extr.anv))
 
@@ -261,7 +277,7 @@ class PostgresExporter(object):
 		for attr, column in [
 				('notes', 'notes'),
 				('genres', 'genres'),
-				('styles', 'styles'),]:
+				('styles', 'styles')]:
 			value = getattr(master, attr)
 			if len(value) != 0:
 				values.append(value)
@@ -275,13 +291,16 @@ class PostgresExporter(object):
 
 		for img in master.images:
 			values = (img.imageType, img.height, img.width, master.id)
-			self.runQuery('masters_images',
+			self.runQuery(
+				'masters_images',
 				['type', 'height', 'width', 'master_id'], values)
 
 		if len(master.artists) > 1:
 			for artist in master.artists:
-				self.runQuery('masters_artists',
-					['master_id', 'artist_name'], (master.id, artist))
+				self.runQuery(
+					'masters_artists',
+					['master_id', 'artist_name'],
+					(master.id, artist))
 			for aj in master.artistJoins:
 				artistIdx = master.artists.index(aj.artist1) + 1
 				# The last join relation is not between artists but instead
@@ -290,18 +309,22 @@ class PostgresExporter(object):
 					values = (master.id, aj.join_relation, '', '')  # join relation is between all artists and the album
 				else:
 					values = (master.id, aj.join_relation, aj.artist1, master.artists[artistIdx])
-				self.runQuery('masters_artists_joins',
-					['master_id', 'join_relation', 'artist1', 'artist2'], values)
+				self.runQuery(
+					'masters_artists_joins',
+					['master_id', 'join_relation', 'artist1', 'artist2'],
+					values)
 		else:
 
-			self.runQuery('masters_artists',
+			self.runQuery(
+				'masters_artists',
 				['master_id', 'artist_name'],
 				# use anv if no artist name
 				(master.id, master.artists[0] if master.artists else master.anv))
 
 		for extr in master.extraartists:
 			# decide whether to insert flattened composite roles or take the first one from the tuple
-			self.runQuery('masters_extraartists',
+			self.runQuery(
+				'masters_extraartists',
 				['master_id', 'artist_name', 'roles'],
 				# (master.id, extr.name, flatten(extr.roles)))
 				(master.id, extr.name, map(lambda x: x[0] if type(x) is tuple else x, extr.roles)))
@@ -317,8 +340,9 @@ class SQL_LIST(object):
 		self._conn = conn
 
 	def getquoted(self):
-		return b"'{" + b', '.join(json.dumps(o, ensure_ascii=False).encode('utf-8')
-			                      for o in self._seq) + b"}'"
+		return (b"'{" + b', '.join(
+				json.dumps(o, ensure_ascii=False).encode('utf-8')
+				for o in self._seq) + b"}'")
 
 	def __str__(self):
 		return str(self.getquoted())

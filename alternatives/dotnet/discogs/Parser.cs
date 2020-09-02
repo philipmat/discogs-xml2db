b98b45365a104066e37d8198df55e21cc6027228
philipmat <github@philipm.at>
2020-09-01 20:23:34 -0500

    uses direct identification of nodes in reader

--- alternatives/dotnet/discogs/Parser.cs
+++ alternatives/dotnet/discogs/Parser.cs
@@ -24,9 +24,10 @@ namespace discogs
             _typeName = typeof(T).Name.Split('.')[^1];
         }
 
-        public event EventHandler<ParseEventArgs> OnSucessfulParse = delegate {};
+        public event EventHandler<ParseEventArgs> OnSucessfulParse = delegate { };
 
-        public async Task ParseFileAsync(string fileName) {
+        public async Task ParseFileAsync(string fileName)
+        {
             using FileStream fileStream = new FileStream(fileName, FileMode.Open, FileAccess.Read, FileShare.Read, bufferSize: BufferSize, useAsync: true);
             Stream readingStream = fileStream;
             if (System.IO.Path.GetExtension(fileName).Equals(".gz", StringComparison.OrdinalIgnoreCase))
@@ -37,7 +38,9 @@ namespace discogs
             await readingStream.DisposeAsync();
         }
 
-        public async Task ParseStreamAsync(Stream stream)
+        public Task ParseStreamAsync(Stream stream) => ParseStreamAsync2(stream);
+
+        public async Task ParseStreamAsync1(Stream stream)
         {
             int objectCount = 0;
             var settings = new XmlReaderSettings
@@ -78,6 +81,77 @@ namespace discogs
                     // await reader.SkipAsync();
                     if (objectCount % _throttle == 0) OnSucessfulParse(null, new ParseEventArgs { ParseCount = objectCount });
                     continue;
+                }
+                else
+                {
+                    await reader.ReadAsync();
+                }
+            }
+            await _exporter.CompleteExportAsync(objectCount);
+        }
+        public async Task ParseStreamAsync2(Stream stream)
+        {
+            int objectCount = 0;
+            var settings = new XmlReaderSettings
+            {
+                ConformanceLevel = ConformanceLevel.Fragment,
+                Async = true,
+            };
+            using XmlReader reader = XmlReader.Create(stream, settings);
+
+            await reader.MoveToContentAsync();
+            await reader.ReadAsync();
+            while (!reader.EOF)
+            {
+                if (reader.Name == _typeName)
+                {
+                    var lbl = new discogs.Labels.label();
+                    while (reader.Read())
+                    {
+                        if (reader.IsStartElement(_typeName))
+                            break;
+                        if (reader.IsStartElement("id"))
+                            lbl.id = reader.ReadElementContentAsString();
+                        if (reader.IsStartElement("name"))
+                            lbl.name = reader.ReadElementContentAsString();
+                        if (reader.IsStartElement("contactinfo"))
+                            lbl.contactinfo = reader.ReadElementContentAsString();
+                        if (reader.IsStartElement("profile"))
+                            lbl.profile = reader.ReadElementContentAsString();
+                        if (reader.IsStartElement("data_quality"))
+                            lbl.data_quality = reader.ReadElementContentAsString();
+                        if (reader.IsStartElement("parentLabel"))
+                            lbl.parentLabel = new discogs.Labels.parentLabel { id = reader.GetAttribute("id"), name = reader.ReadElementContentAsString() };
+                        if (reader.IsStartElement("images"))
+                        {
+                            var images = new List<image>();
+                            while (reader.Read() && reader.IsStartElement("image"))
+                            {
+                                var image = new image { type = reader.GetAttribute("type"), width = reader.GetAttribute("width"), height = reader.GetAttribute("height") };
+                                images.Add(image);
+                            }
+                            lbl.images = images.ToArray();
+                        }
+                        if (reader.IsStartElement("urls"))
+                        {
+                            reader.Read();
+                            var urls = new List<string>();
+                            while (reader.IsStartElement("url"))
+                            {
+                                var url = reader.ReadElementContentAsString();
+                                if (!string.IsNullOrWhiteSpace(url))
+                                    urls.Add(url);
+                            }
+                            lbl.urls = urls.ToArray();
+                        }
+                    }
+                    if (lbl.id is null)
+                        continue;
+                    var obj = (T)(object)lbl;
+                    await _exporter.ExportAsync(obj);
+
+                    objectCount++;
+                    if (objectCount % _throttle == 0) OnSucessfulParse(null, new ParseEventArgs { ParseCount = objectCount });
                 }
                 else
                 {

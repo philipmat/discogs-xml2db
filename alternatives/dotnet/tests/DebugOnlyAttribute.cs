ae921636b2dbe56acb30791520b23b6d8b007ed8
Philip Mateescu <github@philipm.at>
2026-02-07 22:34:06 +0800

    fixes xUnit3003 warning

--- alternatives/dotnet/tests/DebugOnlyAttribute.cs
+++ alternatives/dotnet/tests/DebugOnlyAttribute.cs
@@ -1,8 +1,14 @@
+using System.Runtime.CompilerServices;
+
 namespace tests;
 
 public class DebugOnlyAttribute : FactAttribute
 {
-    public DebugOnlyAttribute()
+    public DebugOnlyAttribute(
+        [CallerFilePath]
+        string? sourceFilePath = null,
+        [CallerLineNumber]
+        int sourceLineNumber = -1) : base(sourceFilePath, sourceLineNumber)
     {
         if (!System.Diagnostics.Debugger.IsAttached)
         {

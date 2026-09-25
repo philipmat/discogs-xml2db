5c639815b314125b4e61df15d0cc6ad7e68aecc5
Philip M <github@philipm.at>
2026-09-25 09:46:38 -0500

    updated urls and wget2 support
    also handles a target folder

--- get_latest_dumps.sh
+++ get_latest_dumps.sh
@@ -1,20 +1,51 @@
 #/bin/bash
 #set -xv
 
-USER_AGENT="Mozilla/5.0 (Macintosh; Intel Mac OS X 10_7_2) AppleWebKit/534.51.22 (KHTML, like Gecko) Version/5.1.1 Safari/534.51.22"
+USER_AGENT="Mozilla/5.0 (compatible; discogs-xml2db/v2.0; +https://github.com/philipmat/discogs-xml2db)"
 ACCEPT="Accept-Encoding: gzip, deflate"
-D_URL_LIST="http://discogs-data.s3-us-west-2.amazonaws.com/?delimiter=/&prefix=data/"$(date +"%Y")"/"
-D_URL_DIR="http://discogs-data.s3-us-west-2.amazonaws.com/data/"$(date +"%Y")"/"
+D_URL_LIST="https://data.discogs.com/?prefix=data%2F"$(date +"%Y")"%2F"
+D_URL_DIR="https://data.discogs.com/data/"$(date +"%Y")"/"
 D_TMP=/tmp/discogs.urls
 D_PATTERN="discogs_[0-9]{8}_(artists|labels|masters|releases).xml.gz"
 
+WGET=wget
+command -v wget2 >/dev/null 2>&1 && WGET=wget2
+
 TEST=""
-[[ "$1" == '--test' ]] && TEST='--spider -S'
-
-echo "" > $D_TMP
-
-for f in `wget -c --user-agent="$USER_AGENT" --header="$ACCEPT" -qO- $D_URL_LIST | grep -Eio "$D_PATTERN" | sort | uniq | tail -n 4` ; do
-	echo $D_URL_DIR$f >> $D_TMP
+DOWNLOAD_DIR=""
+for arg in "$@"; do
+	if [ "$arg" = '--test' ]; then
+		TEST='--spider -S'
+	else
+		DOWNLOAD_DIR=$arg
+	fi
 done
 
-wget -c --user-agent="$USER_AGENT" --header="$ACCEPT" --no-clobber --input-file=$D_TMP $TEST --progress=bar
+if [ -n "$DOWNLOAD_DIR" ]; then
+	mkdir -p "$DOWNLOAD_DIR" || exit 1
+fi
+
+: > "$D_TMP"
+
+$WGET -c --user-agent="$USER_AGENT" --header="$ACCEPT" -O- "$D_URL_LIST" | grep -Eio "$D_PATTERN" | sort | uniq | tail -n 4 | while IFS= read -r f; do
+	echo "$D_URL_DIR$f" >> "$D_TMP"
+done
+
+if [ ! -s "$D_TMP" ]; then
+	echo "No dump files found; check the Discogs dump listing URL or network connection." >&2
+	exit 1
+fi
+
+if [ -n "$TEST" ]; then
+	if [ -n "$DOWNLOAD_DIR" ]; then
+		$WGET -c --user-agent="$USER_AGENT" --header="$ACCEPT" --no-clobber --input-file="$D_TMP" -P "$DOWNLOAD_DIR" --spider -S --progress=bar
+	else
+		$WGET -c --user-agent="$USER_AGENT" --header="$ACCEPT" --no-clobber --input-file="$D_TMP" --spider -S --progress=bar
+	fi
+else
+	if [ -n "$DOWNLOAD_DIR" ]; then
+		$WGET -c --user-agent="$USER_AGENT" --header="$ACCEPT" --no-clobber --input-file="$D_TMP" -P "$DOWNLOAD_DIR" --progress=bar
+	else
+		$WGET -c --user-agent="$USER_AGENT" --header="$ACCEPT" --no-clobber --input-file="$D_TMP" --progress=bar
+	fi
+fi
